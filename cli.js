85f3701f4cee020131071b623d3eb8a710d435e3
anny_more <anxxxen@ya.ru>
2021-04-03 21:19:57 +0300

    fix problems

--- cli.js
+++ cli.js
@@ -3,6 +3,6 @@ import readlineSync from 'readline-sync';
 const getName = () => {
   const name = readlineSync.question('May I have your name?: ');
   const greeting = `Hello, ${name}!`;
-  return greeting
+  return greeting;
 };
 export default getName();
