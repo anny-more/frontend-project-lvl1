ca277cb01283e40603aa6d952b2631cd0224645b
anny_more <anxxxen@ya.ru>
2021-04-03 14:11:33 +0300

    add function getName

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node
 console.log('Welcome to the Brain Games!');
+import getName from '../cli.js';
+console.log(getName);
---
+++ cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+const getName = () => {
+const name = readlineSync.question('May I have your name?: ');
+const greeting = `Hello, ${name}!`;
+	return greeting
+};
+export default getName();
